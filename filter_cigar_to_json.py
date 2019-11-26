abfcddf60ccf52144a54d26edc7b3cde0017efd6
Shuai <shuaixie@zju.edu.cn>
2019-11-26 23:16:43 +0800

    correct filter cigar to json

--- filter_cigar_to_json.py
+++ filter_cigar_to_json.py
@@ -2,6 +2,7 @@ import os
 import json
 import re
 from pprint import pprint
+from collections import OrderedDict
 
 """
 从 207 导出的 json 中选出标注中包含香烟的行
@@ -27,13 +28,29 @@ def filter_cigars_to_json():
                 has_cigar = False
                 for ann in anns:
                     cat = ann['label'][0]
-                    if re.match('.+_[A-Z]', cat) or re.match('.+_[a-z]', cat):
+                    # 要设置结尾匹配，不然 BIG_ROLL 这样也会匹配到
+                    if re.match('^.+_[A-Z]$', cat) or re.match('^.+_[a-z]$', cat):
                         has_cigar = True
                         cnt += 1
                         break
                 if has_cigar:
                     fw.write(line)
         print('cigar img:', cnt)
+
+
+def descend_cat_by_num(cat_num):
+    cat_list = sorted(cat_num.items(), key=lambda t: t[1], reverse=True)
+    print(cat_list)
+    cat_list = [c[0] for c in cat_list]
+    return cat_list
+
+
+def sort_dict_by_val(a_dict, descend=True):
+    b_dict = OrderedDict()
+    a_list = sorted(a_dict.items(), key=lambda t: t[1], reverse=descend)
+    for key, value in a_list:
+        b_dict[key] = value
+    return b_dict
 
 
 def get_cigar_cats_and_other_cats():
@@ -50,21 +67,30 @@ def get_cigar_cats_and_other_cats():
             for ann in anns:
                 cat = ann['label'][0]
                 product_num[cat] = product_num.get(cat, 0) + 1  # default=0
-    print('product cats:', len(product_num))
-    pprint(product_num)
+    # print('product cats:', len(product_num))
+    # product_num = sort_dict_by_val(product_num)
+    # pprint(product_num)
 
     # divide product_num by cigars and others
     cigar_num, other_num = {}, {}
     for cat, num in product_num.items():
-        if re.match('.+_[A-Z]', cat) or re.match('.+_[a-z]', cat):
+        # 要设置结尾匹配，不然 BIG_ROLL 这样也会匹配到
+        if re.match('^.+_[A-Z]$', cat) or re.match('^.+_[a-z]$', cat):
             cigar_num[cat] = num
         else:
             other_num[cat] = num
 
     print('\ncigar cats:', len(cigar_num))
+    cigar_num = sort_dict_by_val(cigar_num)
     pprint(cigar_num)
     print('\nother cats:', len(other_num))
+    other_num = sort_dict_by_val(other_num)
     pprint(other_num)
+
+    cigar_cats = descend_cat_by_num(cigar_num)  # str elem
+    other_cats = descend_cat_by_num(other_num)
+    print(','.join(cigar_cats))  # dataturks label 格式
+    print(','.join(other_cats))
 
 
 if __name__ == '__main__':
